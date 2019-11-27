fd8487ca6b6fb5283f572eeff0857d19b7790a70
fibo <casati_gianluca@yahoo.it>
2019-11-27 22:25:31 +0100

    Handle iframe height on resize

--- src/Root.js
+++ src/Root.js
@@ -1,22 +1,57 @@
-import React, { useState } from 'react'
+import classnames from 'classnames'
+import React, { useEffect, useRef, useState } from 'react'
+
+const navHeight = 56
 
 export default function Root () {
+  const inputRef = useRef(null)
+
+  const [active, setActive] = useState(false)
+  const [height, setHeight] = useState(window.innerHeight - navHeight)
   const [src, setSrc] = useState('')
+
+  useEffect(() => {
+    function handleResize () {
+      setHeight(window.innerHeight - navHeight);
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [setHeight])
 
   return (
     <>
       <nav>
-        <input
-          onBlur={(event) => {
-            setSrc(event.target.value)
+        <form
+          onSubmit={(event) => {
+            event.preventDefault()
+
+            setSrc(inputRef.current.value)
           }}
-          type='text'
-        />
+        >
+          <div className={classnames('field', { active })}>
+            <label>
+              Your URL
+            </label>
+
+            <input
+              onBlur={(event) => {
+                setActive(false)
+
+                setSrc(event.target.value.trim())
+              }}
+              onFocus={() => {
+                setActive(true)
+              }}
+              ref={inputRef}
+              type='text'
+            />
+          </div>
+        </form>
       </nav>
 
-      <div id='preview'>
-        <iframe src={src} />
-      </div>
+      <iframe height={height} src={src} />
     </>
   )
 }
